0432ac0f4770f4933ce0846758113633a5040e79
spenc <spencermayy@gmail.com>
2021-12-20 20:55:50 +0000

    Single trade checking logic

--- app/repository/alpaca/models/TradeValidator.py
+++ app/repository/alpaca/models/TradeValidator.py
@@ -3,28 +3,29 @@ from app.test import test_trade
 
 
 def maximum_checker(value: float) -> bool:
-    return value < 1
+    return value <= 1
 
 
 def minimum_checker(value: float) -> bool:
-    return value >= 0
+    return value > 0
 
 
-def handle_inval_weights(is_valid_min: bool, is_valid_max: bool):
+def handle_inval_weights(is_valid_min: bool, is_valid_max: bool, symbol:str, weight: float):
     if not is_valid_min:
-        raise ValueError("Input Weight must be positive")
+        raise ValueError(symbol + " weight must be positive, is currently: " + str("{:.2%}".format(weight)))
     if not is_valid_max:
-        raise ValueError("Input Weight must be less than 1")
+        raise ValueError(symbol + "Input Weight must be less than 1, is currently: " + str("{:.2%}".format(weight)))
 
 
 def _is_buy_valid(instruction: Instruction) -> bool:
     desired_weight = instruction.weight
+    symbol = instruction.symbol
     # Check desired weight isn't mumbo jumbo
     is_valid_min = minimum_checker(desired_weight)
     is_valid_max = maximum_checker(desired_weight)
 
     if not is_valid_min or not is_valid_max:
-        handle_inval_weights(is_valid_min, is_valid_max)
+        handle_inval_weights(is_valid_min, is_valid_max, symbol, desired_weight)
     return True
 
 
