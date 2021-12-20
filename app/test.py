fd27e76af99708b76bb361b007e17c868846c368
spenc <spencermayy@gmail.com>
2021-12-20 20:56:00 +0000

    Added account handler

--- app/test.py
+++ app/test.py
@@ -1,15 +1,4 @@
-import alpaca_trade_api as tradeapi
-from app.config.config import paper_url, paper_key, paper_secret
+from app.repository.alpaca.models.Instructions import Instruction
 
 
-api = tradeapi.REST(base_url = paper_url, key_id=paper_key, secret_key=paper_secret, api_version='v2')
-
-# Get our account information.
-account = api.get_account()
-
-# Check if our account is restricted from trading.
-if account.trading_blocked:
-    print('Account is currently restricted from trading.')
-
-# Check how much money we can use to open new positions.
-print('${} is available as buying power.'.format(account.buying_power))
+test_trade = Instruction(symbol="GE", side = "buy", weight=0.25, type="market")
