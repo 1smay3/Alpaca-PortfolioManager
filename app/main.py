e2c08c743fb5053a98600e5f4dc50cd6ad3d6742
spenc <spencermayy@gmail.com>
2021-12-23 23:03:02 +0000

    Fixed Widge compliation error

--- app/main.py
+++ app/main.py
@@ -4,16 +4,17 @@ from app.config.constants import DEFAULT_DATE_TIME_FORMAT, DEFAULT_LOG_LOCATION
 from app.logging.logger import Logger
 from app.repository.alpaca.OrderHandler import PortfolioManager
 from app.repository.alpaca.AccountHandler import AccountHandler
-from app.test import portfolio_hardcode
+
 
 # Initiate Classes
-portfolio_manager = PortfolioManager()
 account_handler = AccountHandler()
 logger = Logger(
     date_time_format=DEFAULT_DATE_TIME_FORMAT,
     log_location=DEFAULT_LOG_LOCATION,
     log_level=logging.INFO,
 )
+
+portfolio_manager = PortfolioManager(logger)
 
 # Get / Refresh the current account
 account_handler.pull_account()
