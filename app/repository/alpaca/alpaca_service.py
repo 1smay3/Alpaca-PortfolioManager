e309ae6f0ad93bafc04418da9fb37c3987da3060
Nathan May <nathanonex@gmail.com>
2022-01-08 17:15:43 +0000

    AUTOPM-2 Remove interface from alpaca service as it adds no value

--- app/repository/alpaca/alpaca_service.py
+++ app/repository/alpaca/alpaca_service.py
@@ -1,10 +1,8 @@
 import alpaca_trade_api as tradeapi
 import rx
-from typing import Protocol
 
 from alpaca_trade_api.common import URL
 from rx import Observable
-from abc import abstractmethod
 from app.config.secrets import paper_key_id, paper_key_secret
 from app.repository.alpaca.models.Instructions import Instruction
 
@@ -16,52 +14,28 @@ api = tradeapi.REST(
 )
 
 
-class IAlpacaService(Protocol):
-
-    @abstractmethod
-    def get_clock(self) -> Observable:
-        pass
-
-    @abstractmethod
-    def get_active_assets(self) -> Observable:
-        pass
-
-    @abstractmethod
-    def get_orders(self) -> Observable:
-        pass
-
-    @abstractmethod
-    def get_account(self) -> Observable:
-        pass
-
-    @abstractmethod
-    def get_positions(self) -> Observable:
-        pass
-
-    @abstractmethod
-    def submit_order(self, instruction: Instruction) -> Observable:
-        pass
+def get_account() -> Observable:
+    return rx.of(api.get_account())
 
 
-class AlpacaService(IAlpacaService):
+def get_clock() -> Observable:
+    return rx.of(api.get_clock())
 
-    def get_account(self) -> Observable:
-        return rx.of(api.get_account())
 
-    def get_clock(self) -> Observable:
-        return rx.of(api.get_clock())
+def get_active_assets() -> Observable:
+    return rx.of(api.list_assets(status='active'))
 
-    def get_active_assets(self) -> Observable:
-        return rx.of(api.list_assets(status='active'))
 
-    def get_orders(self) -> Observable:
-        return rx.of(api.list_orders())
+def get_orders() -> Observable:
+    return rx.of(api.list_orders())
 
-    def get_positions(self) -> Observable:
-        return rx.of(api.list_positions())
 
-    def submit_order(self, instruction: Instruction) -> Observable:
-        return rx.of(api.submit_order(instruction.symbol,
-                                      side=instruction.side,
-                                      notional=instruction.weight,
-                                      type=instruction.type))
+def get_positions() -> Observable:
+    return rx.of(api.list_positions())
+
+
+def submit_order(instruction: Instruction) -> Observable:
+    return rx.of(api.submit_order(instruction.symbol,
+                                  side=instruction.side,
+                                  notional=instruction.weight,
+                                  type=instruction.type))
