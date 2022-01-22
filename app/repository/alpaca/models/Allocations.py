c28e3edcb4488c281afbb2e4b3f4eb2ef91414ab
spenc <spencermayy@gmail.com>
2022-01-22 16:12:22 +0000

    Working implementation of weights checking * Next Steps: Handle Trade generator and sizing, and then passing to trading

--- app/repository/alpaca/models/Allocations.py
+++ app/repository/alpaca/models/Allocations.py
@@ -1,5 +1,11 @@
 from dataclasses import dataclass
 
+"""
+https://stackoverflow.com/questions/54140585/how-can-i-use-a-listcustomclass-as-type-with-dataclass-in-python-3-7-x
+https://www.onlinetutorialspoint.com/python/how-to-create-python-iterable-class.html#:~
+:text=An%20iterator%20is%20an%20object,be%20iterate%20over%20the%20values.
+"""
+
 
 # TODO: Convert to strict dataclass as per merged implementation
 @dataclass()
@@ -9,7 +15,14 @@ class Allocation:
     approval_status: bool = False
 
 
-@dataclass()
+# TODO: Need class type which can hold a list of portfolio allocations
 class Portfolio:
-    portfolio_allocations = list[Allocation]
-    approval_status: bool = False
+    def __init__(self, portfolio_allocations, portfolio_approval_status):
+        self.portfolio_allocations = portfolio_allocations
+        self.portfolio_approval_status = portfolio_approval_status
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self
